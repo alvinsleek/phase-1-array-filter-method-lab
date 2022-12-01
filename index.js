2bb25b149c7cabb45bd622be699b17ff125895e4
Alvin Mithamo <alvinmithamo100@gmail.com>
2022-12-01 16:45:12 +0300

    array filter lab

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Code your solution here
+let driverNames = [
+    {
+        name: "Michael"
+        hometown:"Nakuru"
+        
+    },
+    {
+        name: "John"
+        hometown:"Mombasa"
+    },
+    {
+        name: "Michael"
+        hometown:"Kisumu"
+    },
+    {
+        name: "Maureen"
+        hometown:"Marsabit"
+       
+    },
+    {
+        name: "James"
+        hometown:"Isiolo"
+    },
+    {
+        name: "Jonathan"
+        hometown:"Kisii"
+        
+    },
+];
+
+const findMatch = (driverNames, string) => {
+    return driverNames.filter(driver =>driverNames.toLowerCase() === string.toLowerCase())
+}
+console.log(findMatch(driverNames,'Michael'))
+
+function fuzzyMatch(driverNames,query){
+    const results = [];//initialize an empty array to store the results 
+    for(let i=0;i<driverNames.length;i++){
+        if(driverNames[i].startsWith(query)){
+            results.push(driverNames[i]);
+        }
+    }
+    return results;
+}
+console.log(matches);
+
+const matchName = (array, string) => {
+    return array.filter(driver => driver.name === string)
+}
